0f846a6702d9760626e77fa22f55f7ca393bd087
sayyarhassancqu <143068449+sayyarhassancqu@users.noreply.github.com>
2024-04-12 20:50:06 +1000

    Add files via upload

---
+++ Groot/src/main/java/SDD/project/Groot/GrootApplication.java
@@ -0,0 +1,13 @@
+package SDD.project.Groot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrootApplication.class, args);
+	}
+
+}
---
+++ Groot/src/test/java/SDD/project/Groot/GrootApplicationTests.java
@@ -0,0 +1,13 @@
+package SDD.project.Groot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
